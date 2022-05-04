a3a589f7b6e7da657aa5eebbb44ee2e0d2559c85
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-05-04 23:40:54 +0300

    Navigating Programmatically

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
   return (
-    <div>Home</div>
-  )
+    <>
+      <div>Home</div>
+      <button onClick={() => navigate("order-summary", { replace: true })}>
+        Place order
+      </button>
+    </>
+  );
 }
 
-export default Home
+export default Home;
