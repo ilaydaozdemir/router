0d128af2dd56c4a930fdf7c3183fa8150e6fcfb7
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-05-09 11:29:24 +0300

    step-4

--- src/components/RequireAuth.jsx
+++ src/components/RequireAuth.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "./auth";
 
 export const RequireAuth = ({ children }) => {
   const auth = useAuth();
+  const location = useLocation();
 
   if (!auth.user) {
-    return <Navigate to="/login" />;
+    return <Navigate to="/login" state={{ path: location.pathname }} />;
   }
   return children;
 };
